b08297e5ed9baf430ca8bf3944259c127871d0c5
bulubuch <bulubuch@gmail.com>
2023-12-25 22:58:00 +0100

    add products db

---
+++ server/db/products.sql
@@ -0,0 +1,8 @@
+drop table products;
+CREATE TABLE products (id integer primary key autoincrement, name text, unit text, picture text);
+insert into products (name, unit, picture) values ('ail', 'piece', '');
+insert into products (name, unit, picture) values ('aillet', 'botte', '');
+insert into products (name, unit, picture) values ('echalote', 'kg', '');
+insert into products (name, unit, picture) values ('oignon', 'botte', '');
+insert into products (name, unit, picture) values ('oignon de garde', 'kg', '');
+
